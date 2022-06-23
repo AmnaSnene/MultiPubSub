878a0ca38ab78013be8a06b0d31c687065241902
Amna Snene <asnene>
2022-06-23 16:33:36 +0100

    Add publisher and subscriber examples

--- example_scenario_1.py
+++
@@ -1,9 +0,0 @@
-from multipubsub.multi_pub_sub import PubSub
-
-subscribers = PubSub(client_nb=4)
-subscribers.topics = ['PUBLISH TO 1883 BROKER/']
-# subscribers.duration_to_unsubscribe = 30
-
-subscribers.run_multiple(6, pub_or_sub="pub")
-
-subscribers.run_multiple()
---
+++ example_subscriber.py
@@ -0,0 +1,14 @@
+from multipubsub.multi_pub_sub import PubSub
+
+# create PubSub object with 4 client to run later.
+subscribers = PubSub(client_nb=4)
+# specify the topics to subscribe to.
+subscribers.topics = ['PUBLISH TO 1883 BROKER/', 'topic/']
+# unsubscribe after 30 seconds
+subscribers.duration_to_unsubscribe = 30
+# disconnect after 60 seconds
+subscribers.duration_to_disconnect = 60
+
+# run clients: subscribers.
+# the first subscriber id  is client_6, the second is client_7, the third is client_8 and the fourth is client_9.
+subscribers.run_multiple(6, pub_or_sub="sub")
